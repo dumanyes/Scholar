aba0c5fc80c2ddc97c7ffc16084d107442acaa76
dumanyyes <yessenbayduman22@gmail.com>
2025-02-21 12:44:31 +0500

    first

--- user_management/settings.py
+++ user_management/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'scholar-d6c0.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
